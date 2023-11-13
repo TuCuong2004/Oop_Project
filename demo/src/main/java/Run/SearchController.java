634d239a59d12ce09739e8503cbe5311b403657f
TuCuong2004 <125444494+TuCuong2004@users.noreply.github.com>
2023-11-13 22:02:20 +0700

    search.fxml

--- demo/src/main/java/Run/SearchController.java
+++ demo/src/main/java/Run/SearchController.java
@@ -1,4 +1,22 @@
 package Run;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class SearchController {
+    public void handleClickSoundBtn(ActionEvent event) {
+
+    }
+
+    public void handleClickEditBtn(ActionEvent event) {
+    }
+
+    public void handleClickDeleteBtn(ActionEvent event) {
+    }
+
+    public void handleMouseClickAWord(MouseEvent mouseEvent) {
+    }
+
+    public void handleClickSaveBtn(ActionEvent event) {
+    }
 }
