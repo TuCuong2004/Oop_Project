f0e4121e18d17f4be8f0e94b746c825aa0e4778e
TuCuong2004 <125444494+TuCuong2004@users.noreply.github.com>
2023-12-02 23:24:18 +0700

    game1

--- demo/src/main/java/Run/GameController.java
+++ demo/src/main/java/Run/GameController.java
@@ -1,17 +1,16 @@
 package Run;
 
+import java.io.FileNotFoundException;
 import java.util.*;
 
-import DictionaryApplication.Question;
-import DictionaryApplication.Word;
+import DictionaryApplication.DictionaryManagement;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 import static Run.App.dictionaryCommandline;
 public class GameController {
@@ -42,29 +41,29 @@ public class GameController {
     @FXML
     Button button12;
     @FXML
-    TextField textField1;
+    TextArea textField1;
     @FXML
-    TextField textField2;
+    TextArea textField2;
     @FXML
-    TextField textField3;
+    TextArea textField3;
     @FXML
-    TextField textField4;
+    TextArea textField4;
     @FXML
-    TextField textField5;
+    TextArea textField5;
     @FXML
-    TextField textField6;
+    TextArea textField6;
     @FXML
-    TextField textField7;
+    TextArea textField7;
     @FXML
-    TextField textField8;
+    TextArea textField8;
     @FXML
-    TextField textField9;
+    TextArea textField9;
     @FXML
-    TextField textField10;
+    TextArea textField10;
     @FXML
-    TextField textField11;
+    TextArea textField11;
     @FXML
-    TextField textField12;
+    TextArea textField12;
     @FXML
     Pane pane1;
     @FXML
@@ -89,19 +88,23 @@ public class GameController {
     Pane pane11;
     @FXML
     Pane pane12;
-
+    int selectWordId=0;
+    int selectPaneId=1;
     Map<String, Integer> wordExplain = new HashMap<>();
     Map<String , Integer> wordTarget = new HashMap<String, Integer>();
     Map<Integer, Boolean> wordIsSelect = new HashMap<>();
-    List<String> wordList = new ArrayList<>(dictionaryCommandline.getWordlist().keySet());
+    DictionaryManagement  dictionaryManagement = new DictionaryManagement();
+//    Map<Integer,Boolean> paneIsVisible = new HashMap<>();
 
-    public void insertNewWord() {
+    public void insertNewWord() throws FileNotFoundException {
+        dictionaryManagement.insertFromFilePath();
+        List<String> wordList = new ArrayList<>(dictionaryManagement.getWordlist().keySet());
         wordIsSelect.put(0, true);
         for(int i=0; i<6; i++) {
             Random random = new Random();
             int randomNumber = 0;
             while (wordIsSelect.containsKey(randomNumber)) {
-                randomNumber = random.nextInt(103931);
+                randomNumber = random.nextInt(2914);
             }
             wordTarget.put(wordList.get(randomNumber), randomNumber);
             wordExplain.put(dictionaryCommandline.getWordlist().get(wordList.get(randomNumber)).getWord_explain(), randomNumber);
@@ -168,10 +171,291 @@ public class GameController {
             i2--;
         }
     }
-    public void gameStart () {
+    public void game() throws FileNotFoundException {
+
         insertNewWord();
         setButton();
-        pane1.setVisible(false);
-        textField2.setEditable(false);
     }
+
+    public void gameStart() throws FileNotFoundException {
+        insertNewWord();
+        setButton();
+
+//        for(int i=1; i<=12; i++){
+//            paneIsVisible.put(i,true);
+//        }
+        pane1.setVisible(false);
+        pane2.setVisible(false);
+        pane3.setVisible(false);
+        pane4.setVisible(false);
+        pane5.setVisible(false);
+        pane6.setVisible(false);
+        pane7.setVisible(false);
+        pane8.setVisible(false);
+        pane9.setVisible(false);
+        pane10.setVisible(false);
+        pane11.setVisible(false);
+        pane12.setVisible(false);
+        textField7.setWrapText(true);
+        textField8.setWrapText(true);
+        textField9.setWrapText(true);
+        textField10.setWrapText(true);
+        textField11.setWrapText(true);
+        textField12.setWrapText(true);
+
+    }
+    public Pane getSelectedPane() {
+        switch (selectPaneId) {
+            case 1:
+                return pane1;
+            case 2:
+                return pane2;
+            case 3:
+                return pane3;
+            case 4:
+                return pane4;
+            case 5:
+                return pane5;
+            case 6:
+                return pane6;
+            case 7:
+                return pane7;
+            case 8:
+                return pane8;
+            case 9:
+                return pane9;
+            case 10:
+                return pane10;
+            case 11:
+                return pane11;
+            case 12:
+                return pane12;
+            default:
+                return null;
+        }
+    }
+
+    public TextArea getSelectedTextField() {
+        switch (selectPaneId) {
+            case 1: return textField1;
+            case 2: return textField2;
+            case 3: return textField3;
+            case 4: return textField4;
+            case 5: return textField5;
+            case 6: return textField6;
+            case 7: return textField7;
+            case 8: return textField8;
+            case 9: return textField9;
+            case 10: return textField10;
+            case 11: return textField11;
+            case 12: return textField12;
+            default: return null;
+        }
+    }
+
+    public Button getSelectedButton() {
+        switch (selectPaneId) {
+            case 1: return button1;
+            case 2: return button2;
+            case 3: return button3;
+            case 4: return button4;
+            case 5: return button5;
+            case 6: return button6;
+            case 7: return button7;
+            case 8: return button8;
+            case 9: return button9;
+            case 10: return button10;
+            case 11: return button11;
+            case 12: return button12;
+            default: return null;
+        }
+    }
+    public  void clickOnButton1() {
+        if(wordTarget.get(textField1.getText()) == selectWordId && selectPaneId>6) {
+            textField1.setVisible(false);
+            button1.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        }
+        else {
+            getSelectedPane().setVisible(false);
+            pane1.setVisible(true);
+            selectPaneId = 1;
+            selectWordId = wordTarget.get(textField1.getText());
+        }
+    }
+
+    public void clickOnButton2() {
+        if(wordTarget.get(textField2.getText()) == selectWordId && selectPaneId>6) {
+            textField2.setVisible(false);
+            button2.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        }
+        else {
+            getSelectedPane().setVisible(false);
+            pane2.setVisible(true);
+            selectPaneId = 2;
+            selectWordId = wordTarget.get(textField2.getText());
+        }
+    }
+
+    public void clickOnButton3() {
+        if(wordTarget.get(textField3.getText()) == selectWordId && selectPaneId>6) {
+            textField3.setVisible(false);
+            button3.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        }
+        else {
+            getSelectedPane().setVisible(false);
+            pane3.setVisible(true);
+            selectPaneId = 3;
+            selectWordId = wordTarget.get(textField3.getText());
+        }
+    }
+
+    public void clickOnButton4() {
+        if(wordTarget.get(textField4.getText()) == selectWordId && selectPaneId>6) {
+            textField4.setVisible(false);
+            button4.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        }
+        else {
+            getSelectedPane().setVisible(false);
+            pane4.setVisible(true);
+            selectPaneId = 4;
+            selectWordId = wordTarget.get(textField4.getText());
+        }
+    }
+
+    public void clickOnButton5() {
+        if(wordTarget.get(textField5.getText()) == selectWordId && selectPaneId>6) {
+            textField5.setVisible(false);
+            button5.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        }
+        else {
+            getSelectedPane().setVisible(false);
+            pane5.setVisible(true);
+            selectPaneId = 5;
+            selectWordId = wordTarget.get(textField5.getText());
+        }
+    }
+
+    public void clickOnButton6() {
+        if(wordTarget.get(textField6.getText()) == selectWordId && selectPaneId>6) {
+            textField6.setVisible(false);
+            button6.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        }
+        else {
+            getSelectedPane().setVisible(false);
+            pane6.setVisible(true);
+            selectPaneId = 6;
+            selectWordId = wordTarget.get(textField6.getText());
+        }
+    }
+
+
+    public void clickOnButton7() {
+        if (wordExplain.get(textField7.getText()) == selectWordId && selectPaneId<=6) {
+            textField7.setVisible(false);
+            button7.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        } else {
+            getSelectedPane().setVisible(false);
+            pane7.setVisible(true);
+            selectPaneId = 7;
+            selectWordId = wordExplain.get(textField7.getText());
+        }
+    }
+
+    public void clickOnButton8() {
+        if (wordExplain.get(textField8.getText()) == selectWordId && selectPaneId<=6) {
+            textField8.setVisible(false);
+            button8.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        } else {
+            getSelectedPane().setVisible(false);
+            pane8.setVisible(true);
+            selectPaneId = 8;
+            selectWordId = wordExplain.get(textField8.getText());
+        }
+    }
+
+    public void clickOnButton9() {
+        if (wordExplain.get(textField9.getText()) == selectWordId && selectPaneId<=6) {
+            textField9.setVisible(false);
+            button9.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        } else {
+            getSelectedPane().setVisible(false);
+            pane9.setVisible(true);
+            selectPaneId = 9;
+            selectWordId = wordExplain.get(textField9.getText());
+        }
+    }
+
+    public void clickOnButton10() {
+        if (wordExplain.get(textField10.getText()) == selectWordId && selectPaneId<=6) {
+            textField10.setVisible(false);
+            button10.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        } else {
+            getSelectedPane().setVisible(false);
+            pane10.setVisible(true);
+            selectPaneId = 10;
+            selectWordId = wordExplain.get(textField10.getText());
+        }
+    }
+
+    public void clickOnButton11() {
+        if (wordExplain.get(textField11.getText()) == selectWordId && selectPaneId<=6) {
+            textField11.setVisible(false);
+            button11.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        } else {
+            getSelectedPane().setVisible(false);
+            pane11.setVisible(true);
+            selectPaneId = 11;
+            selectWordId = wordExplain.get(textField11.getText());
+        }
+    }
+    public void clickOnButton12() {
+        if (wordExplain.get(textField12.getText()) == selectWordId && selectPaneId<=6) {
+            textField12.setVisible(false);
+            button12.setVisible(false);
+            getSelectedPane().setVisible(false);
+            getSelectedTextField().setVisible(false);
+            getSelectedButton().setVisible(false);
+        } else {
+            getSelectedPane().setVisible(false);
+            pane12.setVisible(true);
+            selectPaneId = 12;
+            selectWordId = wordExplain.get(textField12.getText());
+        }
+    }
+
+
+
 }
